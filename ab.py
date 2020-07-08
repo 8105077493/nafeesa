6639f77c3ecc4c891c6443d7bf7a95cd9ddc7b8f
8105077493 <67917164+8105077493@users.noreply.github.com>
2020-07-08 21:45:46 +0530

    Add files via upload

---
+++ ab.py
@@ -0,0 +1,12 @@
+employees={1:'john',2:'raj'}
+del employees[2]
+print (employees)
+
+numbers1=[1,2,3,4]
+numbers2=[5,6,7,8]
+print (numbers1[1:])
+
+tuple=('a','b','c','d')
+
+print(tuple[0])
+print(tuple[3])
